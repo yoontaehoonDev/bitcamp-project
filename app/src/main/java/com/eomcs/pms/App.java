0c6f2196f0f71571914db457886cad2cdf929925
yoontaehoonDev <siryou10@gmail.com>
2021-01-25 20:27:48 +0900

    up

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,225 +1,102 @@
 package com.eomcs.pms;
 
-import java.sql.Date;
-import java.util.Scanner;
+import com.eomcs.pms.handler.BoardHandler;
+import com.eomcs.pms.handler.MemberHandler;
+import com.eomcs.pms.handler.ProjectHandler;
+import com.eomcs.pms.handler.TaskHandler;
+import com.eomcs.util.Prompt;
 
 public class App {
 
-  static Scanner keyboardScan = new Scanner(System.in);
-  static final int MEMBER_LENGTH = 100;
-  static final int PROJECT_LENGTH = 100;
-  static final int TASK_LENGTH = 100;
-
-  static int[] no = new int[MEMBER_LENGTH];
-  static String[] name = new String[MEMBER_LENGTH];
-  static String[] email = new String[MEMBER_LENGTH];
-  static String[] password = new String[MEMBER_LENGTH];
-  static String[] photo = new String[MEMBER_LENGTH];
-  static String[] tel = new String[MEMBER_LENGTH];
-  static Date[] registeredDate = new Date[MEMBER_LENGTH];
-
-  static int[] pNo = new int[PROJECT_LENGTH];
-  static String[] pTitle = new String[PROJECT_LENGTH];
-  static String[] pContent = new String[PROJECT_LENGTH];
-  static Date[] pStartDate = new Date[PROJECT_LENGTH];
-  static Date[] pEndDate = new Date[PROJECT_LENGTH];
-  static String[] pOwner = new String[PROJECT_LENGTH];
-  static String[] pMembers = new String[PROJECT_LENGTH];
-
-  static int[] tNo = new int[TASK_LENGTH];
-  static String[] tContent = new String[TASK_LENGTH];
-  static Date[] tDeadline = new Date[TASK_LENGTH];
-  static String[] tOwner = new String[TASK_LENGTH];
-  static int[] tStatus = new int[TASK_LENGTH];
-
-  static int mCount = 0;
-  static int pCount = 0;
-  static int tCount = 0;
-
-  static String response;
-  static String input;
-
-  static void addMember() {
-    System.out.println("[멤버 등록]");
-    for (int i = mCount; i < MEMBER_LENGTH; i++) {
-      System.out.print("번호? ");
-      no[i] = Integer.parseInt(keyboardScan.nextLine());
-
-      System.out.print("이름? ");
-      name[i] = keyboardScan.nextLine();
-
-      System.out.print("이메일? ");
-      email[i] = keyboardScan.nextLine();
-
-      System.out.print("암호? ");
-      password[i] = keyboardScan.nextLine();
-
-      System.out.print("사진? ");
-      photo[i] = keyboardScan.nextLine();
-
-      System.out.print("전화? ");
-      tel[i] = keyboardScan.nextLine();
-
-      registeredDate[i] = new java.sql.Date(System.currentTimeMillis());
-
-      mCount++;
-
-      System.out.println();
-
-      System.out.print("계속 입력하시겠습니까?(y/N) ");
-      response = keyboardScan.nextLine();
-      if (!response.equalsIgnoreCase("y")) {
-        break;
-      }
-      System.out.println(); 
-    }
-  }
-
-  static void listMember() {
-    System.out.println("[멤버 목록]");
-    for (int i = 0; i < mCount; i++) {
-      System.out.printf("%d, %s, %s, %s, %s\n",
-          no[i], name[i], email[i], tel[i], registeredDate[i]);
-    }
-  }
-
-  static void addProject() {
-    System.out.println("[프로젝트 등록]");
-    for (int i = pCount; i < PROJECT_LENGTH; i++) {
-      System.out.print("번호? ");
-      pNo[i] = Integer.valueOf(keyboardScan.nextLine());
-
-      System.out.print("프로젝트명? ");
-      pTitle[i] = keyboardScan.nextLine();
-
-      System.out.print("내용? ");
-      pContent[i] = keyboardScan.nextLine();
-
-      System.out.print("시작일? ");
-      pStartDate[i] = Date.valueOf(keyboardScan.nextLine());
-
-      System.out.print("종료일? ");
-      pEndDate[i] = Date.valueOf(keyboardScan.nextLine());
-
-      System.out.print("만든이? ");
-      pOwner[i] = keyboardScan.nextLine();
-
-      System.out.print("팀원? ");
-      pMembers[i] = keyboardScan.nextLine();
-
-      pCount++;
-      System.out.println();
-
-      System.out.print("계속 입력하시겠습니까?(y/N) ");
-      response = keyboardScan.nextLine();
-      if (!response.equalsIgnoreCase("y")) {
-        break;
-      }
-      System.out.println();
-    }
-  }
-
-  static void listProject() {
-    System.out.println("[프로젝트 목록]");
-    for (int i = 0; i < pCount; i++) {
-      System.out.printf("%d, %s, %s, %s, %s\n",
-          pNo[i], pTitle[i], pStartDate[i], pEndDate[i], pOwner[i]);
-    }
-  }
-
-  static void addTask() {
-    System.out.println("[작업 등록]");
-    for (int i = tCount; i < TASK_LENGTH; i++) {
-      System.out.print("번호? ");
-      tNo[i] = Integer.parseInt(keyboardScan.nextLine());
-
-      System.out.print("내용? ");
-      tContent[i] = keyboardScan.nextLine();
-
-      System.out.print("마감일? ");
-      tDeadline[i] = Date.valueOf(keyboardScan.nextLine());
-
-      System.out.println("상태?");
-      System.out.println("0: 신규");
-      System.out.println("1: 진행중");
-      System.out.println("2: 완료");
-      System.out.print("> ");
-      tStatus[i] = Integer.valueOf(keyboardScan.nextLine());
-
-      System.out.print("담당자? ");
-      tOwner[i] = keyboardScan.nextLine();
-
-      tCount++;
-      System.out.println();
-
-      System.out.print("계속 입력하시겠습니까?(y/N) ");
-      response = keyboardScan.nextLine();
-      if (!response.equalsIgnoreCase("y")) {
-        break;
-      }
-      System.out.println();
-    }
-  }
-
-  static void listTask() {
-    System.out.println("[작업 목록]");
-    for (int i = 0; i < tCount; i++) {
-      String stateLabel = null;
-      switch (tStatus[i]) {
-        case 1:
-          stateLabel = "진행중";
-          break;
-        case 2:
-          stateLabel = "완료";
-          break;
-        default:
-          stateLabel = "신규";
-      }
-      System.out.printf("%d, %s, %s, %s, %s\n",
-          tNo[i], tContent[i], tDeadline[i], stateLabel, tOwner[i]);
-    }
-  }
-
   public static void main(String[] args) {
 
-    while(true) {
-      System.out.print("명령 > ");
-      input = keyboardScan.nextLine();
-      if(input.equalsIgnoreCase("quit") || input.equalsIgnoreCase("exit")) {
-        break;
-      }
-      else if (input.equalsIgnoreCase("/member/add")) {
-        addMember();
-      }
-      else if (input.equalsIgnoreCase("/member/list")) {
-        listMember();
-      }
-      else if (input.equalsIgnoreCase("/project/add")) {
-        addProject();
-      }
-      else if (input.equalsIgnoreCase("/project/list")) {
-        listProject();
-      }
-      else if (input.equalsIgnoreCase("/task/add")) {
-        addTask();
-      }
-      else if (input.equalsIgnoreCase("/task/list")) {
-        listTask();
-      }
-      else {
-        System.out.println("실행할 수 없는 명령어입니다.");
+    // 각 게시판 데이터를 저장할 메모리 준비
+    BoardHandler boardList1 = new BoardHandler();
+    BoardHandler boardList2 = new BoardHandler();
+    BoardHandler boardList3 = new BoardHandler();
+    BoardHandler boardList4 = new BoardHandler();
+    BoardHandler boardList5 = new BoardHandler();
+    BoardHandler boardList6 = new BoardHandler();
+
+    // 각 회원 목록 데이터를 저장할 메모리 준비
+    MemberHandler memberList = new MemberHandler();
+
+    // 각 프로젝트 목록 데이터를 저장할 메모리 준비
+    ProjectHandler projectList = new ProjectHandler();
+    projectList.memberList = memberList;
+    // 각 작업 목록 데이터를 저장할 메모리 준비
+    TaskHandler taskList = new TaskHandler();
+    taskList.memberList = memberList;
+
+    loop:
+      while (true) {
+        String command = com.eomcs.util.Prompt.inputString("명령> ");
+
+        switch (command) {
+          case "/member/add":
+            memberList.add();
+            break;
+          case "/member/list":
+            memberList.list();
+            break;
+          case "/project/add":
+            projectList.add();
+            break;
+          case "/project/list":
+            projectList.list();
+            break;
+          case "/task/add":
+            taskList.add();
+            break;
+          case "/task/list":
+            taskList.list();
+            break;
+          case "/board/add":
+            boardList1.add();
+            break;
+          case "/board/list":
+            boardList1.list();
+            break;
+          case "/board2/add":
+            boardList2.add();
+            break;
+          case "/board2/list":
+            boardList2.list();
+            break;
+          case "/board3/add":
+            boardList3.add();
+            break;
+          case "/board3/list":
+            boardList3.list();
+            break;
+          case "/board4/add":
+            boardList4.add();
+            break;
+          case "/board4/list":
+            boardList4.list();
+            break;
+          case "/board5/add":
+            boardList5.add();
+            break;
+          case "/board5/list":
+            boardList5.list();
+            break;
+          case "/board6/add":
+            boardList6.add();
+            break;
+          case "/board6/list":
+            boardList6.list();
+            break;
+          case "quit":
+          case "exit":
+            System.out.println("안녕!");
+            break loop;
+          default:
+            System.out.println("실행할 수 없는 명령입니다.");
+        }
+        System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
       }
 
-      System.out.println();
-    }
-
-
-
-
-    keyboardScan.close();
-
-    System.out.println("--------------------------------");
-
-
+    Prompt.close();
   }
 }
